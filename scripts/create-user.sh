c60b534f8dc5a899200eb250c73366a45c04ae40
Dmitry Marov <d.marov94@gmail.com>
2019-08-16 17:03:21 +0300

    update

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+SCRIPTFULLPATH=$(realpath $0);
+BASEDIR=$(dirname $SCRIPTFULLPATH);
+
 pw useradd user -m -s /usr/local/bin/zsh -u 1001
+
+cp $BASEDIR/assets/sudoers /usr/local/etc/sudoers
